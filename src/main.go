66820271ad505d9303873ab4f59023bbd5b9431c
13524008-Agatha <agatha2ndemail@gmail.com>
2026-02-15 16:10:05 +0700

    feat(validator): implemented and tested validator for board

--- src/main.go
+++ src/main.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	// parse
 	b, err := parse_board_from_file("input.txt")
 	if err != nil {
 		fmt.Println("ERR:", err)
 		return
 	}
+
+	// validate
+	if err := validate_board(b); err != nil {
+		fmt.Println("INVALID BOARD:", err)
+		return
+	}
+
 	fmt.Println("N:", b.n)
 	fmt.Println("ID Unique:", b.idUnique)
 	for i := 0; i < b.n; i++ {
